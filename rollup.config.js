ce1e0002ab74f1ef65dda13d13348b50b69d202f
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-09 16:14:44 +0100

    config: removed import css

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import css from "rollup-plugin-import-css"
 import { terser } from "rollup-plugin-terser";
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
@@ -29,7 +28,6 @@ export default [
             commonjs(),
             typescript({ tsconfig: "./tsconfig.json" }),
             terser(),
-            css()
         ],
         external: ["react", "react-dom", "styled-components"]
     },
